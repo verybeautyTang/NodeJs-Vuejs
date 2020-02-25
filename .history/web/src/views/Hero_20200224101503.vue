9092783d271aab7f7226fd4b203c0f21d2753c07
verybeautyTang <353332175@qq.com>
2020-02-25 09:42:27 +0800

    thrid

---
+++ .history/web/src/views/Hero_20200224101503.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    {{hero.name}}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    Id: {required: true}
+  },
+  data () {
+    return {
+      hero: null
+    }
+  },
+  methods: {
+    async fetch () {
+      const res = this.$http.get(`heroes/${this.Id}`)
+      this.hero = res.data
+    }
+  },
+  created () {
+    this.fetch()
+  }
+}
+
+</script>
+<style scoped>
+</style>
---
+++ .history/web/src/views/Hero_20200224165100.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="hero_list">
+    <div class="bg-black tarbar px-3 d-flex ai-center">
+      <img class="m-1" src="../assets/logo.png" height="35">
+        <div class="mt-3 text-white">王者荣耀</div>
+      <div class="flex-1  pl-5">
+        <div class=" text-white mt-3" >攻略站</div>
+      </div>
+      <div class=" text-white mt-3" >更多英雄 &gt;</div>
+    </div>
+    <div class="hero_bg" :style="{'background-image':`url(${hero.banner})`}">
+      <div class="hero_title">
+        <div class="info text-white">
+          <div>{{hero.title}}</div>
+          <div>{{hero.name}}</div>
+          <div>{{hero.categories.map (v => v.name).join('/')}}</div>
+        </div>
+        <div class=" d-flex text-white p-2">
+          <span>难度</span>
+          <div class="skills">{{hero.scores.difficult}}</div>
+          <span>技能</span>
+          <div class="skills ">{{hero.scores.skills}}</div>
+          <span>攻击</span>
+          <div class="skills ">{{hero.scores.attack}}</div>
+          <span>生存</span>
+          <div class="skills ">{{hero.scores.survive}}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {required: true}
+  },
+  data () {
+    return {
+      hero: null
+    }
+  },
+  methods: {
+    async fetch () {
+      const res = await this.$http.get(`heroes/${this.id}`)
+      this.hero = res.data
+      console.log(res.data)
+    }
+  },
+  created () {
+    this.fetch()
+  }
+}
+
+</script>
+<style lang='scss'>
+.hero_list {
+  .hero_bg{
+    background-size: auto 100%;
+    height: 50vw;
+    background-repeat: no-repeat;
+    background-position: top right;
+    background-color: #fff;
+  }
+}
+</style>
